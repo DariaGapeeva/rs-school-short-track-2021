13ec3abd178358f70e8760a8455c02044e7d16e9
DariaGapeeva <d.gapeeva@gmail.com>
2021-04-12 15:00:04 +0700

    feat: implement task 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -11,29 +11,52 @@
  *
  */
 
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 
 class Queue {
-  // constructor() {
-  //   this.stack = new ListNode();
-  // }
-  get size() {
-    throw new Error('Not implemented');
-    //   let tmp = this.stack
-    //   let counter = 0
-    //   while (tmp) {
-    //     counter++;
-    //     tmp=tmp.next
-    //   }
-    //   return counter
+  constructor() {
+    this.stack = new ListNode();
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    //   throw new Error('Not implemented');
+    let tmp = this.stack;
+    let counter = 0;
+    while (tmp) {
+      counter++;
+      tmp = tmp.next;
+    }
+    return counter;
+  }
+
+  enqueue(element) {
+    //   throw new Error('Not implemented');
+    let tmp = this.stack;
+    if (tmp.value === undefined) {
+      tmp.value = element;
+    } else {
+      while (tmp.next) {
+        tmp = tmp.next;
+      }
+
+      tmp.next = new ListNode(element);
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    //   throw new Error('Not implemented');
+
+    const thisNode = this.stack;
+    const result = thisNode.value;
+    if (thisNode.next === null) {
+      this.stack = new ListNode();
+    } else {
+      const nextNode = thisNode.next;
+      thisNode.next = nextNode.next;
+      thisNode.value = nextNode.value;
+    }
+
+    return result;
   }
 }
 
